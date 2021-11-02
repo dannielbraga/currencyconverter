e29d654faab6e04f00fdb71588dcf7406830e68f
Danniel Dias Braga <dannieldiasbraga@gmail.com>
2021-11-01 21:36:12 -0300

    Initial project structure created

---
+++ src/main/java/br/com/jayatech/currencyconverter/CurrencyconverterApplication.java
@@ -0,0 +1,13 @@
+package br.com.jayatech.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jayatech/currencyconverter/CurrencyconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jayatech.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
